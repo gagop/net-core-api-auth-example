bd5c6cc65dca4440eaa66cb7aef7ac645452046c
Piotr Gago <gagopiotr@gmail.com>
2020-10-18 19:30:31 +0200

    Changed the Startup file for .NET Core 3.1

--- NetCoreApiExample/NetCoreApiExample/Startup.cs
+++ NetCoreApiExample/NetCoreApiExample/Startup.cs
@@ -43,7 +43,8 @@ namespace NetCoreApiExample
                         };
                     });
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddControllers();
+            services.AddMvc(options => options.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
